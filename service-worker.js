83c8878e66495a61023dd2cbe0feafaebf5d5484
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2020-04-02 09:09:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-charts/precache-manifest.5c04d7be0028017d6b912974c12e09a9.js"
+  "/covid-charts/precache-manifest.d53718ca9f93162179eaa49fc9ff8066.js"
 );
 
 self.addEventListener('message', (event) => {
